943b8a89ac039d577bb1f84dc3cc46a91f1e833e
Jeff Lyon <jeff@albatrossdigital.com>
2014-09-05 12:28:16 -0700

    Updating directives

--- js/directives.js
+++ js/directives.js
@@ -7,3 +7,32 @@ justiceApp.directive('sidebar', function(){
         templateUrl: 'partials/sidebar.html',
     };
 });
+justiceApp.directive('shortUrl', function(){
+    return {
+        restrict: 'A',
+        replace: true,
+        scope: { field:'=field' },
+        //controller: 'ShortenCtrl',
+        template: '<a href="{{url}}" title="{{url}}" target="_blank">{{shortUrl}}</a>',
+        link: function(scope, element, attrs, ctrl) {
+            var updateLink = function(url) {
+                if (url != undefined) {
+                    scope.url = url;
+                    scope.shortUrl = url.length > 50 ? url.substring(0,50) + '...' : url;
+                }
+            }
+
+            scope.$watch('field', function(field) {
+                updateLink(field);
+            });
+        }
+    }
+});
+
+
+/*
+$("#FN, #LN").each (function () {
+  if ($(this).text().length > 50)
+    $(this).text($(this).text().substring(0,50) + '...'));
+});
+*/
